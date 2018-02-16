0dbaf1351437af4455896de154d5e61522203e69
freeman <alexguo@gmail.com>
2018-02-16 15:26:26 +0800

    add libWeChatSDK

---
+++ WechatOpenSDK/WechatOpenSDK.h
@@ -0,0 +1,21 @@
+//
+//  WechatOpenSDK.h
+//  WechatOpenSDK
+//
+//  Created by freeman on 16/2/2018.
+//  Copyright © 2018 freeman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WXApi.h"
+#import "WXApiObject.h"
+#import "WechatAuthSDK.h"
+
+//! Project version number for WechatOpenSDK.
+FOUNDATION_EXPORT double WechatOpenSDKVersionNumber;
+
+//! Project version string for WechatOpenSDK.
+FOUNDATION_EXPORT const unsigned char WechatOpenSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WechatOpenSDK/PublicHeader.h>
---
+++ WechatOpenSDK/dummy.swift
@@ -0,0 +1,12 @@
+//
+//  test.swift
+//  WechatOpenSDK
+//
+//  Created by freeman on 16/2/2018.
+//  Copyright © 2018 freeman. All rights reserved.
+//
+
+public func dummy() {
+    print(WXApi.getVersion())
+}
+
